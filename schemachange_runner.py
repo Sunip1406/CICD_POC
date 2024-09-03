665f234397776e4328df9ebf0343b2caf5a40379
Sunip Das <sunipdas@ymail.com>
2024-09-03 12:23:46 +0530

    schemachange changes

--- schemachange_runner.py
+++ schemachange_runner.py
@@ -3,23 +3,50 @@ import json
 import sys
 import os
 
+def read_template(template_path):
+    with open('schemachange.template.yml', 'r') as file:
+        return file.read()
+
 def load_config(stage):
     with open('config/env_config.json', 'r') as f:
         config = json.load(f)
     return config[stage]
 
+def write_config(config_content, output_path):
+    with open(output_path, 'w') as file:
+        file.write(config_content)
+
+def replace_placeholders(template_content, config_data):
+    for key, value in config_data.items():
+        placeholder = f'{{{{{key}}}}}'
+        if value is None:
+            raise ValueError(f'Value for {key} not set in config file')
+        template_content = template_content.replace(placeholder, value)
+    return template_content
+
+def main():
+    # Read the template file
+    template_content = read_template(template_path)
+
+    # Read the configuration JSON file
+    config_data = load_config(stage)
+
+    # Replace placeholders with values from the configuration file
+    config_content = replace_placeholders(template_content, config_data)
+
+    # Write the resulting configuration to the output file
+    write_config(config_content, output_path)
+
+if __name__ == '__main__':
+    main()
+
 def run_schemachange(stage):
     config = load_config(stage)
     
     command = [
-        "schemachange",
-        "-f", "snowflake_changes",  # Path to the change scripts
-        "--account", config["snowflake_account"],
-        "-u", config["snowflake_user"],
-        "--password", config["snowflake_password"],
-        "-d", config["snowflake_database"],
-        "-s", config["snowflake_schema"],
-        "--change-history-table", f"{config['snowflake_database']}.{config['snowflake_schema']}.change_history"  
+        "schemachange apply",
+        "--config","CICD_POC/schemachange.yml",
+        "-f", "snowflake_changes"  # Path to the change scripts
              ]
     
     try:
